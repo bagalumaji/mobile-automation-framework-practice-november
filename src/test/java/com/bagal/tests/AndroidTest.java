85ee6560aa3ab02408b18387cdd16070339f8e85
bagalumaji <bagalumaji@gmail.com>
2023-11-27 20:36:51 -0800

    added adroid test

---
+++ src/test/java/com/bagal/tests/AndroidTest.java
@@ -0,0 +1,24 @@
+package com.bagal.tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.Platform;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidTest {
+    @Test
+    public void androidLaunchDemoTest() throws MalformedURLException, InterruptedException {
+        UiAutomator2Options uiAutomator2Options = new UiAutomator2Options();
+        uiAutomator2Options.setPlatformName("Android");
+        uiAutomator2Options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        uiAutomator2Options.setDeviceName("Sayaji Test Device");
+        uiAutomator2Options.setApp(System.getProperty("user.dir")+"/src/test/resources/apps/AndroidMyDemoAppRN.apk");
+        AndroidDriver androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"),uiAutomator2Options);
+        androidDriver.quit();
+
+    }
+}
